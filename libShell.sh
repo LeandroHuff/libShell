4bb13c63889700ed1bc2178160c05bafdf21b2bc
LeandroHuff <leandrohuff@programmer.net>
2025-09-01 23:49:48 -0300

    update on file libShell

--- libShell.sh
+++ libShell.sh
@@ -1,5 +1,3 @@
-#!/usr/bin/env bash
-
 ################################################################################
 # @brief        Shell Script Library.                                          #
 # @file         libShell                                                       #
